c3bcd45ecab6075621b8f039581e30dbfc0b3810
UEM <sohamdas0625@gmail.com>
2025-03-21 12:50:32 +0530

    pushed in my profile

--- oops/Employee.java
+++ oops/Employee.java
@@ -11,12 +11,23 @@ public class Employee {
 		this.phoneNo = phoneNo;
 	}
 	
+	Employee(String name, double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	Employee(String name) {
+		this.name = name;
+	}
+	
 	public static void main(String[] args) {
 		Employee p1 = new Employee("Gaurav Basu", 20000, 9876451229l);
-		Employee p2 = new Employee("Ritika Roy", 23000, 9674652566l);
+		Employee p2 = new Employee("Ritika Roy", 23000);
+		Employee p3 = new Employee("Soham Das");
 		
 		System.out.println(p1.name+"\t"+p1.salary+"\t"+p1.phoneNo);
-		System.out.println(p2.name+"\t"+p2.salary+"\t"+p2.phoneNo);
+		System.out.println(p2.name+"\t"+p2.salary);
+		System.out.println(p3.name);
 	}
 
 }
