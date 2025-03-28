eedcf805aa6605772da7d39b9af3c6afc77ac743
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-03-29 02:47:03 +0530

    Delete PaymentProcessingSystem.java

--- PaymentProcessingSystem.java
+++
@@ -1,9 +0,0 @@
-public class PaymentProcessingSystem {
-    public static void main(String[] args) {
-        Payment creditCardPayment = new CreditCardPayment(600.0, 12345);
-        creditCardPayment.processPayment();
-
-        Payment paypalPayment = new PayPalPayment(300.0, 67890);
-        paypalPayment.processPayment();
-    }
-}
